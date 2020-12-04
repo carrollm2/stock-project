9fab8a1c0e00f893ec6b480d7aef1ae93464394e
carrollm2 <carrollm2@gmail.com>
2020-12-04 18:11:33 -0500

    add STOCK_DATA variable to create action

--- stock-backend/app/controllers/ratios_controller.rb
+++ stock-backend/app/controllers/ratios_controller.rb
@@ -1,9 +1,46 @@
 class RatiosController < ApplicationController
 
+    STOCK_DATA = {
+        "FB" => {
+            "current ratio" => 5.509,
+            "gross margin" => 80.4658,
+            "net profit margin" => 36.544,
+            "return on equity" => 6.6643,
+            "return on assets" => 5.3579,
+            "free cashflow per share" => 2.1122
+        },
+        "AAPL" => {
+            "current ratio" => 1.3636,
+            "gross margin" => 38.1604,
+            "net profit margin" => 19.5879,
+            "return on equity" => 19.3958,
+            "return on assets" => 3.9128,
+            "free cashflow per share" => 1.0526
+        },
+        "NFLX" => {
+            "current ratio" => 1.2376,
+            "gross margin" => 39.901,
+            "net profit margin" => 12.275,
+            "return on equity" => 7.6449,
+            "return on assets" => 2.0454,
+            "free cashflow per share" => 2.5298
+        },    
+
+        "GOOGL" => {
+            "current ratio" => 3.4101,
+            "gross margin" => 54.2655,
+            "net profit margin" => 24.3584,
+            "return on equity" => 5.2823,
+            "return on assets" => 3.7585,
+            "free cashflow per share" => 38.8358
+        }
+    }
+
+
     def create
-        binding.pry
+
         stock = Stock.find(params[:stock_id])
-        ratio = stock.ratios.build(name: "debt to equity ratio", value: 0.06)
+        ratio = stock.ratios.build(name: params[:ratio_name], value: STOCK_DATA[stock.ticker][params[:ratio_name]])
 
         if ratio.save
             render json: ratio
