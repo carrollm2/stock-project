5b679e6e2e066224129634c6b5fccc26bb41c690
carrollm2 <carrollm2@gmail.com>
2020-12-30 17:57:05 -0500

    add favorited column to stocks table

--- stock-backend/db/schema.rb
+++ stock-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_025547) do
+ActiveRecord::Schema.define(version: 2020_12_30_205029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_11_14_025547) do
     t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "favorited", default: false
   end
 
   add_foreign_key "ratios", "stocks"
