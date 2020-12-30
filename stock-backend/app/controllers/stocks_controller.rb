ae27bc25290688c3573b91da7d5990b652de4aea
carrollm2 <carrollm2@gmail.com>
2020-12-30 17:56:32 -0500

    add favorited to strong params

--- stock-backend/app/controllers/stocks_controller.rb
+++ stock-backend/app/controllers/stocks_controller.rb
@@ -30,7 +30,7 @@ class StocksController < ApplicationController
 
     private
     def stock_params
-        params.require(:stock).permit(:ticker, :price, ratios_attributes: [:id, :name, :value])
+        params.require(:stock).permit(:ticker, :price, :favorited, ratios_attributes: [:id, :name, :value])
     end  
 
     def set_stock
